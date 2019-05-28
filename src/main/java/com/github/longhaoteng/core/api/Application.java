31ec01c5c602888b051e7461f6972c5f9ed779b3
longhaoteng <lht19941025@163.com>
2019-05-29 02:02:18 +0800

    adjust configuration format

--- src/main/java/com/github/longhaoteng/core/api/Application.java
+++ src/main/java/com/github/longhaoteng/core/api/Application.java
@@ -2,6 +2,7 @@ package com.github.longhaoteng.core.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class Application {
 
-    @Value("${api.auth}")
+    @Value("${spring.api.auth}")
     private String auth;
 
     @Autowired
@@ -20,10 +21,11 @@ public class Application {
     @PostMapping(value = "/api", consumes = "application/json", produces = "application/json")
     public Response api(HttpServletRequest httpServletRequest, @RequestBody Request request) {
         String auth = httpServletRequest.getHeader("auth");
-        if (auth != null && auth.contains(auth)) {
-            request.setServlet(httpServletRequest);
+        request.setServlet(httpServletRequest);
+        if (StringUtils.isEmpty(this.auth)) {
             return apiEngine.handle(request);
+        } else {
+            return this.auth.equals(auth) ? apiEngine.handle(request) : null;
         }
-        return null;
     }
 }
